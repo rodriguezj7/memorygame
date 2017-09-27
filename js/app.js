9aba0b1469d9c2b7c6f6a62cf43c2198e9025d91
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-09-26 23:30:04 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,78 @@
+/*
+ * Create a list that holds all of your cards
+ */
+const deck = document.getElementById("deck");
+var cards = deck.childNodes;
+var allCards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor','fa fa-bolt','fa fa-cube', 'fa fa-anchor', 'fa fa-leaf','fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf',
+'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o','fa fa-cube'];
+var scoreCount = document.getElementsByClassName("score-panel");
+var starScore = '<li><i class="fa fa-star"></i></li>';
+var repeat = document.getElementById("restart");
+//build card
+var cardInfo = function(index, array){
+	for(let x = 0; x < index; x++){
+	let cardHTML = document.createElement("li");
+	let cardType = document.createElement('i')
+	deck.appendChild(cardHTML);
+	cardHTML.setAttribute("class", "card");
+	cardHTML.appendChild(cardType);
+	cardType.setAttribute("class", array[x]);
+	}
+ }
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+// repeat game or restart
+repeat.addEventListener("click", function(){
+	//new shuffle
+	let newShuffle = shuffle(allCards);
+	newBoard(16, newShuffle);
+	console.log(newShuffle);
+	console.log("Success!");
+});
+
+// Create new board
+function newBoard(cards, array){
+	//clear board
+	var firstNode = deck.firstChild;
+	while(firstNode){
+		deck.removeChild(firstNode);
+		firstNode = deck.firstChild;
+	}
+	//call and build new cards
+	cardInfo(cards, array);
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+cards.forEach(function(){
+	cards.addEventListener("click", function(e){
+		x = iele.className;
+		console.log(x);
+	});
+})
